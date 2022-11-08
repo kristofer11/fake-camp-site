5feaa4673837ee7c46b78b7bb40860041b8fc264
kristofer11 <klhvattum@live.com>
2022-11-07 19:23:34 -0800

    NPM Scripts Part1

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(function() {
+    $(".carousel").carousel( {interval: 2000 });
+    $("#carouselPause").click(function() {
+        $(".carousel").carousel("pause");
+    });
+    $("#carouselPlay").click(function() {
+        $(".carousel").carousel("cycle");
+    })
+});
+
+// $('#reserveModal').modal(options)
